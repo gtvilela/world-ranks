e36c7e2d467f8db09b301bec9f6ec0c702baaafa
GabrielA <gabrielathalitasv@gmail.com>
2021-02-03 20:54:54 -0300

    Correção _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -24,8 +24,6 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <title>My page</title>
-          {/* Step 5: Output the styles in the head  */}
           {this.props.styleTags}
         </Head>
         <body>

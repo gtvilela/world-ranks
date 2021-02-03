ef6f523605e14f34f5267c8ba4ac3d6cc015e26a
GabrielA <gabrielathalitasv@gmail.com>
2021-02-03 14:30:53 -0300

    Concluindo a tela de detalhes do país

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -1,34 +1,113 @@
+import { useEffect, useState } from "react";
 import Layout from "../../components/Layout/Layout";
 import styles from "./country.module.css";
 
+const getCountry = async (id) => {
+  const res = await fetch(`https://restcountries.eu/rest/v2/alpha/${id}`);
+  const country = await res.json();
+
+  return country;
+};
+
 const Country = ({ country }) => {
+  const [borders, setBorders] = useState([]);
+
+  const getBorders = async () => {
+    const borders = await Promise.all(
+      country.borders.map((border) => getCountry(border))
+    );
+
+    setBorders(borders);
+  };
+
+  useEffect(() => {
+    getBorders();
+  }, []);
+
   return (
     <Layout title={country.name}>
-      <div >
-        <div className={styles.overview_panel}>
-          <img src={country.flag} alt={country.name}></img>
+      <div className={styles.container}>
+        <div>
+          <div className={styles.overview_panel}>
+            <img src={country.flag} alt={country.name}></img>
 
-          <h1 className={styles.overview_name}>{country.name}</h1>
-          <div className={styles.overview_region}>{country.region}</div>
+            <h1 className={styles.overview_name}>{country.name}</h1>
+            <div className={styles.overview_region}>{country.region}</div>
 
-          <div className={styles.overview_number}>
-            <div className={styles.overview_population}>
-              <div className={styles.overview_value}>{country.population}</div>
-              <div className={styles.overview_label}>Population</div>
-            </div>
-            <div className={styles.overview_area}>
-              <div className={styles.overview_value}>{country.area}</div>
-              <div className={styles.overview_label}>Area</div>
+            <div className={styles.overview_number}>
+              <div className={styles.overview_population}>
+                <div className={styles.overview_value}>
+                  {country.population}
+                </div>
+                <div className={styles.overview_label}>Population</div>
+              </div>
+              <div className={styles.overview_area}>
+                <div className={styles.overview_value}>{country.area}</div>
+                <div className={styles.overview_label}>Area</div>
+              </div>
             </div>
           </div>
         </div>
 
-        <div className={styles.details_panel}>
+        <div className={styles.container_right}>
+          <div className={styles.details_panel}>
             <h4 className={styles.details_panel_heading}>Details</h4>
-            <div>
-                <div className={styles.details_panel_row}>Captial</div>
-                <div>{country.captial}</div>
+
+            <div className={styles.details_panel_row}>
+              <div className={styles.details_panel_label}>Capital</div>
+              <div className={styles.details_panel_value}>
+                {country.capital}
+              </div>
             </div>
+
+            <div className={styles.details_panel_row}>
+              <div className={styles.details_panel_label}>Subregion</div>
+              <div className={styles.details_panel_value}>
+                {country.subregion}
+              </div>
+            </div>
+
+            <div className={styles.details_panel_row}>
+              <div className={styles.details_panel_label}>Languages</div>
+              <div className={styles.details_panel_value}>
+                {country.languages.map(({ name }) => name).join(", ")}
+              </div>
+            </div>
+
+            <div className={styles.details_panel_row}>
+              <div className={styles.details_panel_label}>Currency</div>
+              <div className={styles.details_panel_value}>
+                {country.currencies.map(({ name }) => name).join(", ")}
+              </div>
+            </div>
+
+            <div className={styles.details_panel_row}>
+              <div className={styles.details_panel_label}>Native name</div>
+              <div className={styles.details_panel_value}>
+                {country.nativeName}
+              </div>
+            </div>
+
+            <div className={styles.details_panel_row}>
+              <div className={styles.details_panel_label}>Gini</div>
+              <div className={styles.details_panel_value}>{country.gini} %</div>
+            </div>
+
+            <div className={styles.details_panel_borders}>
+              <div className={styles.details_panel_borders_label}>Neighbouring Countries</div>
+
+              <div className={styles.details_panel_borders_container}>
+                {borders.map(({ flag, name }) => (
+                  <div className={styles.details_panel_borders_country}>
+                    <img src={flag} alt={name}></img>
+                    <div className={styles.details_panel_borders_name}>
+                      {name}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
@@ -38,10 +117,7 @@ const Country = ({ country }) => {
 export default Country;
 
 export const getServerSideProps = async ({ params }) => {
-  const res = await fetch(
-    `https://restcountries.eu/rest/v2/alpha/${params.id}`
-  );
-  const country = await res.json();
+  const country = await getCountry(params.id);
 
   return {
     props: {
